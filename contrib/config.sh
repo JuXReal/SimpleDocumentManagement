f2229537d5e0fded3897720a1bb9da648cbbb3a4
JuXReal <juxreal@hotmail.com>
2015-01-30 13:17:16 +0100

    update

--- contrib/config.sh
+++ contrib/config.sh
@@ -11,10 +11,9 @@ sdm_dv="$sdm_path"
 sdm_bin="$sdm_path/bin"
 
 #specific script path configuration
-sdm_run="$sdm_path/bin/run.sh"
-sdm_index="$sdm_path/bin/index.sh"
-sdm_webui="$sdm_path/recoll-webui && ./webui-standalone.py -a"
-sdm_clean="$sdm_path/bin/clean.sh"
+sdm_run="$sdm_path/bin/run.sh > /dev/null 2>&1"
+sdm_index="$sdm_path/bin/index.sh > /dev/null 2>&1"
+sdm_clean="$sdm_path/bin/clean.sh > /dev/null 2>&1"
 
 
 #OCRmyPDF configuration
@@ -32,5 +31,5 @@ sdm_user="pi"
 
 sdm_cron1="* * * * * sudo $sdm_run"
 sdm_cron2="*/5 * * * * $sdm_index"
-sdm_cron3="@reboot screen -dmS RecollWebGui bash -c" "cd $sdm_webui"""
-sdm_cron4="@reboot $sdm_clean > /dev/null 2>&1"
+sdm_cron3="@reboot screen -dmS RecollWebGui bash -c "cd $sdm_path/recoll-webui && ./webui-standalone.py -a""
+sdm_cron4="@reboot $sdm_clean"
